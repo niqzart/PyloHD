df0f6f9aab7cbb9d1bc3d6e9e4d504d8315ae0fb
niqzart <qwert45hi@yandex.ru>
2024-01-28 01:32:41 +0300

    init: start on documentation with docusaurus

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,73 @@
+import {themes as prismThemes} from "prism-react-renderer";
+import type {Config} from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "PyloHD",
+  tagline: "High-level framework for simplification and systematization of processes in electronic design",
+  favicon: "img/favicon.ico",
+
+  url: "https://your-docusaurus-site.example.com",
+  baseUrl: "/PyloHD/",
+
+  organizationName: "niqzart",
+  projectName: "PyloHD",
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          routeBasePath: "/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "PyloHD",
+      logo: {
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "docsSidebar",
+          position: "left",
+          label: "Docs",
+        },
+        {
+          type: "docSidebar",
+          sidebarId: "thesisSidebar",
+          position: "left",
+          label: "Thesis",
+        },
+        {
+          href: "https://github.com/niqzart/PyloHD",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
